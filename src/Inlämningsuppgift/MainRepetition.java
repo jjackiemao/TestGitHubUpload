53226fd9a92fb6b1cfc5145170598e4858325bd9
jjackiemao <jjackiemao98@gmail.com>
2023-11-14 13:34:52 +0100

    Test

--- src/Inlämningsuppgift/MainRepetition.java
+++ src/Inlämningsuppgift/MainRepetition.java
@@ -3,5 +3,8 @@ package Inlämningsuppgift;
 public class MainRepetition {
     public static void main(String[] args) {
         System.out.println("Hello Mom! :)");
+        System.out.println("Live long and prosper");
+        System.out.println("Hello dad!");
+        System.out.println("Hello cat!");
     }
 }
